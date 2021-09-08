8e8979ee08cabd4bb1a9be5ea9e3576c991ffe50
Vsevolod-IT <it_vsevolod@yahoo.com>
2021-09-08 15:32:16 +0300

    feat: add basic layout and routing into App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import PageAllPokemons from "./UI/pages/PageAllPokemons";
+import PageCatchedPokemons from "./UI/pages/PageCatchedPokemons";
+import PagePokemon from "./UI/pages/PagePokemon";
+import Header from "./UI/components/Header";
+
+
+function App() {
+   return (
+     <div className="App">
+       <Router>
+         <Header />
+           <Switch>
+             <Route path="/" exact component={PageAllPokemons} />
+             <Route path="/Catch" exact component={PageCatchedPokemons} />
+             <Route path="/Pokemon/:pokemonId" exact component={PagePokemon} />
+             <Route component={NotFound} />
+           </Switch>
+       </Router>
+     </div>
+   )
+ }
+ 
+ export default App;
