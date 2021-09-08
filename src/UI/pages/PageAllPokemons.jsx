8722f54fd91fd192e3d644102d22f8713a8ceeb1
Vsevolod-IT <it_vsevolod@yahoo.com>
2021-09-08 18:56:02 +0300

    feat: implement fetch pokemons

--- src/UI/pages/PageAllPokemons.jsx
+++ src/UI/pages/PageAllPokemons.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchPokemons, loadPokemons } from "../../redux/actions/pokemonsActions";
 
 const PageAllPokemons = () => {
+
+   const [fetch, setFetch] = useState(false);
+   const [currentPage, setCurrent] = useState(2);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(loadPokemons()); 
+   }, []);
+
+   const scrollHandler = (e) => {
+      if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight)< 100) {
+         setFetch(true)
+      }
+   };
+
+   useEffect( () => {
+      document.addEventListener('scroll', scrollHandler)
+      return function() {
+        document.removeEventListener('scroll', scrollHandler)
+      };
+    }, []);
+
+
+   useEffect(() => {
+      if(fetch){
+         dispatch(fetchPokemons(currentPage));
+         setCurrent(prevState => prevState + 1);
+         setFetch(false)
+      } 
+   }, [fetch]);
+   
    return(
       <main>
-
+         PageAllPokemons
       </main>
    )
 };
--- src/redux/actions/pokemonsActions.js
+++ src/redux/actions/pokemonsActions.js
@@ -1,13 +1,18 @@
-import { getPokemons, getPokemon } from "../api/apiRedux/apiAxios";
-import ActionTypes from "./actionsType";
+import { apiAxios } from "../api/apiRedux";
+import { ActionTypes } from "./actionsType";
 
-export const fetchPokemons = () => async (dispatch) => {
+export const loadPokemons = () => async (dispatch) => {
    const firstPage = 1;
-   const response = await getPokemons(firstPage);
-   dispatch({type: ActionTypes.LOAD_FETCH_POKEMONS, payload: response.data});
+   const response = await apiAxios.getPokemons(firstPage);
+   dispatch({type: ActionTypes.LOAD_FETCH_POKEMONS, payload: response});
+}
+
+export const fetchPokemons = (page) => async (dispatch) => {
+   const response = await apiAxios.getPokemons(page);
+   dispatch({type: ActionTypes.FETCH_POKEMONS, payload: response.data});
 }
 
 export const fetchPokemon = (id) => async (dispatch) => {
-   const response = await getPokemon(id);
+   const response = await apiAxios.getPokemon(id);
    dispatch({type: ActionTypes.SELECT_POKEMON, payload: response.data,});
 }
