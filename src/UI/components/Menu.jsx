294ba37e1cc9ac84e828740042161d12c4c34f55
Vsevolod-IT <it_vsevolod@yahoo.com>
2021-09-08 20:04:08 +0300

    refactor: del materilize and add bootstrap

--- src/UI/components/Menu.jsx
+++ src/UI/components/Menu.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const Menu = () => {
    return (
-      <nav>
-         <ul>
-            <li></li>
-         </ul>
-      </nav>
+      <>
+
+<ul id="dropdown2" className="dropdown-content">
+    <li><a href="#!">one<span className="badge">1</span></a></li>
+    <li><a href="#!">two<span className="new badge">1</span></a></li>
+    <li><a href="#!">three</a></li>
+  </ul>
+  <a className="btn dropdown-trigger" href="#!" data-target="dropdown2">Dropdown<i className="material-icons right">arrow_drop_down</i></a>
+      </>
    )
 }
 
